203d884ef3e03d2d41e00ad3167170cc8b056ff8
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-01-02 16:49:50 +0100

    Added node graph

--- project02/gui.js
+++ project02/gui.js
@@ -4,16 +4,48 @@ const join = (acc, str) => acc + str
 const seq  = (n, arr = []) => n < 1 ? arr : seq(n - 1, [n, ...arr])
 
 const gui = new class {
-  graph     = document.getElementById('graph')
-  schedule  = document.getElementById('schedule')
+  graph    = document.getElementById('graph')
+  schedule = document.getElementById('schedule')
 
-  draw = machines => {
-    if (!machines || machines.length === 0) return gui.error('Nie znaleziono maszyn')
+  draw = (machines = []) => {
+    if (machines.length === 0) throw new Error('Nie znaleziono maszyn')
     const tasks = machines.flatMap(m => m.tasks)
 
-    if (tasks.length === 0) return gui.error('Brak zadań')
+    if (tasks.length === 0) throw new Error('Brak zadań')
 
+    this.drawGraph(tasks)
     this.drawSchedule(machines)
+  }
+
+  drawGraph = tasks => {
+    const data = {
+      nodes: tasks.map(task => ({
+        id: task.id,
+        label: task.id,
+        color: '#FAB795'
+      })),
+      edges: tasks.flatMap(task => task.required.map(r => ({
+        from: r.id,
+        to: task.id,
+        arrows: 'to',
+        color: '#E95678'
+      }))),
+      options: {
+        physics: false,
+        layout: {
+          hierarchical: {
+            enabled: true,
+            levelSeparation: 100,
+            nodeSpacing: 75,
+            treeSpacing: 100,
+            direction: 'LR',
+            sortMethod: 'directed',
+            shakeTowards: 'roots'
+          }
+        }
+      }
+    }
+    new vis.Network(this.graph, data)
   }
 
   drawSchedule = (machines = []) => {
@@ -32,8 +64,8 @@ const gui = new class {
 
   makeTaskCell = task => `<td>${task.id}</td>`
 
-  error = (text, error = 'Coś poszło nie tak') => {
+  error = (error) => {
     console.error(error)
-    document.body.innerHTML = `<h1>${text}</h1><p>${error}</p>`
+    document.body.innerHTML = `<h1>${error}</h1>`
   }
 }()
--- project02/main.js
+++ project02/main.js
@@ -1,25 +1,26 @@
 'use strict'
+try {
+  const tasks = Task.parseList([
+    { id:  'Z1' },
+    { id:  'Z2' },
+    { id:  'Z3', required: [ 'Z2'] },
+    { id:  'Z4', required: [ 'Z1',  'Z3'] },
+    { id:  'Z6' },
+    { id:  'Z5', required: [ 'Z1',  'Z2',  'Z6'] },
+    { id:  'Z7', required: [ 'Z1',  'Z3'] },
+    { id:  'Z8', required: [ 'Z3'] },
+    { id:  'Z9', required: [ 'Z4',  'Z5',  'Z7'] },
+    { id: 'Z10', required: [ 'Z5'] },
+    { id: 'Z11', required: [ 'Z7'] },
+    { id: 'Z12', required: [ 'Z5',  'Z6',  'Z8'] },
+    { id: 'Z13', required: [ 'Z6',  'Z8'] },
+    { id: 'Z14', required: [ 'Z7'] },
+    { id: 'Z15', required: [ 'Z9', 'Z10', 'Z11', 'Z12'] },
+    { id: 'Z16', required: ['Z11', 'Z13'] },
+    { id: 'Z18', required: ['Z13', 'Z14'] },
+    { id: 'Z17', required: ['Z10', 'Z12', 'Z16', 'Z18'] },
+    { id: 'Z19', required: ['Z11', 'Z14'] },
+  ])
 
-const tasks = Task.parseList([
-  { id:  'Z1' },
-  { id:  'Z2' },
-  { id:  'Z3', required: [ 'Z2'] },
-  { id:  'Z4', required: [ 'Z1',  'Z3'] },
-  { id:  'Z6' },
-  { id:  'Z5', required: [ 'Z1',  'Z2',  'Z6'] },
-  { id:  'Z7', required: [ 'Z1',  'Z3'] },
-  { id:  'Z8', required: [ 'Z3'] },
-  { id:  'Z9', required: [ 'Z4',  'Z5',  'Z7'] },
-  { id: 'Z10', required: [ 'Z5'] },
-  { id: 'Z11', required: [ 'Z7'] },
-  { id: 'Z12', required: [ 'Z5',  'Z6',  'Z8'] },
-  { id: 'Z13', required: [ 'Z6',  'Z8'] },
-  { id: 'Z14', required: [ 'Z7'] },
-  { id: 'Z15', required: [ 'Z9', 'Z10', 'Z11', 'Z12'] },
-  { id: 'Z16', required: ['Z11', 'Z13'] },
-  { id: 'Z18', required: ['Z13', 'Z14'] },
-  { id: 'Z17', required: ['Z10', 'Z12', 'Z16', 'Z18'] },
-  { id: 'Z19', required: ['Z11', 'Z14'] },
-])
-
-gui.draw(schedule(tasks))
+  gui.draw(schedule(tasks))
+} catch (e) { gui.error(e) }
